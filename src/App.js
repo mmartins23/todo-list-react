7a6d3540b11ac6657e6eb64f5752e0e7b76e50f3
Marlon Martins <marlon-martins@outlook.com>
2023-04-26 13:32:23 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input type='text' id='item'></input>
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1>Todo List</h1>
+
+      <ul className='list'>
+        <li>
+          <label>
+            <input type='checkbox'/>
+            Item 1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
 
