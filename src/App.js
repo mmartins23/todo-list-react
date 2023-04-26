4b61d86eac88d761541d7684fe7c07a367867528
Marlon Martins <marlon-martins@outlook.com>
2023-04-26 16:59:46 +0200

    toggleTodo function complete

--- src/App.js
+++ src/App.js
@@ -20,6 +20,17 @@ function App() {
     setNewItem('');
   }
 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo;
+      })
+    })
+  }
+
 
   return (
     <>
@@ -40,7 +51,11 @@ function App() {
       {todos.map(todo => {
        return <li key={todo.id}>
           <label>
-            <input type='checkbox' checked={todo.completed} />
+            <input 
+            type='checkbox' 
+            checked={todo.completed} 
+            onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+            />
             {todo.title}
           </label>
           <button className='btn btn-danger'>Delete</button>
