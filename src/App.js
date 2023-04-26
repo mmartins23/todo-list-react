fb7786e3c35f180c5cd37bcbb1d409d2da5d867f
Marlon Martins <marlon-martins@outlook.com>
2023-04-26 16:04:25 +0200

    React useState() was added to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [newItem, setNewItem] = useState('');
+
   return (
     <>
       <form className='new-item-form'>
         <div className='form-row'>
           <label htmlFor='item'>New Item</label>
-          <input type='text' id='item'></input>
+          <input 
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type='text' 
+          id='item'></input>
         </div>
         <button className='btn'>Add</button>
       </form>
