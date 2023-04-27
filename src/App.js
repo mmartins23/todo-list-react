7d0fd51cb60f1c4b00f6039ec63e0e86c560f4e3
Marlon Martins <marlon-martins@outlook.com>
2023-04-27 13:10:00 +0200

    UseEffect added to Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NewTodoForm } from './NewTodoForm';
 import { TodoList } from './TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem('ITEMS');
+
+    if (localValue === null) {
+      return []
+    } else {
+      return JSON.parse(localValue);
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('ITEMS', JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title) {
     setTodos(currentTodos => {
